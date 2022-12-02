cf1060a419f8ba215f5f4c22cdb8d7847e767348
jigsaw <m@jgs.me>
2022-12-02 15:15:05 +0900

    fix script

--- scripts/fetch.mjs
+++ scripts/fetch.mjs
@@ -1,4 +1,6 @@
 import { writeFile } from "fs/promises";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
 
 const main = async () => {
   const [res1, res2] = await Promise.all([
@@ -6,7 +8,13 @@ const main = async () => {
     fetch("https://w.kbys.tk/api/recent_clips"),
   ]);
   const [articles, clips] = await Promise.all([res1.json(), res2.json()]);
-  await writeFile("./data/updates.json", JSON.stringify({ articles, clips }));
+  await writeFile(
+    resolve(
+      dirname(fileURLToPath(import.meta.url)),
+      "../src/data/updates.json"
+    ),
+    JSON.stringify({ articles, clips })
+  );
 };
 
 main();

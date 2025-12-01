191b11b8c8463f3e414c5b5f2ec80d6e8b0860fb
jigsaw <m@jgs.me>
2025-12-01 17:20:23 +0900

    fix

--- packages/notify/src/index.ts
+++ packages/notify/src/index.ts
@@ -86,7 +86,7 @@ async function sendDiscordNotification(
       const token = await generateToken(p.id, env.REGISTER_SECRET);
       const registerUrl = `https://${env.SITE_URL}/api/article/register?token=${token}`;
       const excludeUrl = `https://${env.SITE_URL}/api/article/exclude?token=${token}`;
-      return `- ${p.title}\n
+      return `- ${p.title}
   - [登録](${registerUrl})・[除外](${excludeUrl})`;
     })
   );

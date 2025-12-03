f8021709553ee4e26f6c813cc5031dd030653d5e
jigsaw <m@jgs.me>
2025-12-03 21:49:42 +0900

    add ga

--- packages/web/pages/+Head.tsx
+++ packages/web/pages/+Head.tsx
@@ -1,4 +1,5 @@
 const OG_BASE_URL = "https://og.w.jgs.me";
+const GA_ID = "G-SJB62M7N1F";
 
 export function Head() {
   return (
@@ -7,6 +8,20 @@ export function Head() {
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
       <meta name="twitter:card" content="summary_large_image" />
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}');
+          `,
+        }}
+      />
     </>
   );
 }
